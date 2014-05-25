56a23cdb28921374a26f618dbd992e9888e14382
Max Agi <maxagi.tau@gmail.com>
2014-05-25 19:38:31 +0300

    changed mObserevers type

--- Hotel_Manager/Subject.h
+++ Hotel_Manager/Subject.h
@@ -7,12 +7,22 @@ class Subject{
 public:
 	virtual ~Subject();
 	virtual void attach(Observer* o,const int eventType);	//attach an observer for events of type:  eventType
+
 	virtual void notify(const int eventType);				//notify only Observers that observe events of type: eventType
+															//implementation : iterate over mObserevers , if ObserverRecord->eventType == eventType
+															//call ObserverRecord->o->update , else do nothing
+
 	virtual void notify_all();								//notify all observers
 
 protected:
 	Subject();
 
 private:
-	std::list<Observer *> mObserevers;
+
+	struct ObserverRecord{
+		Observer * o;
+		int eventType;
+	};
+
+	std::list<ObserverRecord *> mObserevers;
 };
