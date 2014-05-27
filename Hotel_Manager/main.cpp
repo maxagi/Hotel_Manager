3aab2fe989e76bd8b4ef68cf8a799ccc68caa564
Max Agi <maxagi.tau@gmail.com>
2014-05-27 21:23:44 +0300

    start of main

--- Hotel_Manager/main.cpp
+++ Hotel_Manager/main.cpp
@@ -1,7 +1,34 @@
-
 #include "Hotel.h"
-
+#include<iostream>
+using namespace std;
 
 int main(){
 
+
+	unsigned int c, c1;
+	bool cont = true;
+
+	while (cont){
+		cout << "\n";
+		cout << "Hotel system " << endl
+			<< "0: changes the price of the rooms " << endl
+			<< "1: notifies that HCM clients have to upgrade membership" << endl
+			<< "2: quit the system" << endl
+			<< "Enter your choice : ";
+		cin >> c;
+
+
+		switch (c) {
+		case 0:
+
+			break;
+		case 1:
+
+			break;
+
+
+		default:
+			cont = false;
+		}
+	}
 }
