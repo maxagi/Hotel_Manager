9616eea46511853fc1e1f51bbdf3567c88b65497
Max Agi <maxagi.tau@gmail.com>
2014-05-28 14:29:29 +0300

    interactive main

--- Hotel_Manager/main.cpp
+++ Hotel_Manager/main.cpp
@@ -1,39 +1,50 @@
 #include "Hotel.h"
 #include<iostream>
+#include <vld.h>
 using namespace std;
 
 int main(){
 
+	unsigned int c, c1;
+	bool cont = true;
+	string str;
 
 	Hotel& hotel = Hotel::getInstance();
 
-	hotel.addGuest(Hotel::HEQ, "max");
-	hotel.addGuest(Hotel::HEQ, "hussam");
-	hotel.addGuest(Hotel::HCM, "Yossi Halachmi");
-
-
-
-	unsigned int c, c1;
-	bool cont = true;
 
 	while (cont){
 		cout << "\n";
 		cout << "Hotel system " << endl
 			<< "0: changes the price of the rooms " << endl
 			<< "1: notifies that HCM clients have to upgrade membership" << endl
-			<< "2: quit the system" << endl
+			<< "2: add new HCM guest" << endl
+			<< "3: add new HEQ guest" << endl
+			<< "4: quit the system" << endl
 			<< "Enter your choice : ";
 		cin >> c;
 
 
 		switch (c) {
 		case 0:
-
+			hotel.notify_all();
 			break;
 		case 1:
+			hotel.notify(Hotel::HCM);
+			break;
+		case 2:
+			cin.sync();
+			cout << "please enter the HCM guest name" << endl;
+			getline(cin, str);
+			hotel.addGuest(Hotel::HCM, str);
 
 			break;
 
+		case 3:
+			cin.sync();
+			cout << "please enter the HEQ guest name" << endl;
+			getline(cin, str);
+			hotel.addGuest(Hotel::HEQ, str);
+			break;
 
 		default:
 			cont = false;
